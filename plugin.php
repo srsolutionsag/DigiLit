f5dca12b726daf07c36c42df8e549c0a3456dc19
Florian <fw@studer-raimann.ch>
2018-07-12 15:38:21 +0200

    Plugin version 5.3

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'xdgl';
 $version = '2.1.2';
-$ilias_min_version = '5.1';
-$ilias_max_version = '5.2.99';
+$ilias_min_version = '5.3.0';
+$ilias_max_version = '5.3.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
