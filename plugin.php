534ab4fe9c3083278a44252a72818005ed81703f
Fabian Schmid <fabian@sr.solutions>
2022-01-21 16:26:56 +0100

    plugin version and support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,8 @@
 <?php
 $id = 'xdgl';
-$version = '3.0.0';
-$ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
-$responsible = 'studer + raimann ag';
-$responsible_mail = 'support@studer-raimann.ch';
+$version = '4.0.0';
+$ilias_min_version = '7.0';
+$ilias_max_version = '7.999';
+$responsible = 'sr solutions';
+$responsible_mail = 'support@sr.solutions';
+$supports_cli_setup = false;
